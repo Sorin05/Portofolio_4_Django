59fd8704d1b984a38032fe898b8be13c851711fe
Sorin05 <sorinandrei230587@gmail.com>
2022-07-11 12:22:24 +0000

    Install allauth and style login and logou html

--- blog/views.py
+++ blog/views.py
@@ -12,15 +12,15 @@ class RoutineList(generic.ListView):
 
 
 class RoutineDetail(View):
-    
+
     def get(self, request, slug, *args, **kwargs):
-        queryset = Routine.objects.filter(staus=1)
-        post = get_object_or_404(queryset, slug=slug)
+        queryset = Routine.objects.filter(status=1)
+        routine = get_object_or_404(queryset, slug=slug)
         comments = routine.comments.filter(approved=True).order_by('created_on')
         liked = False
         if routine.likes.filter(id=self.request.user.id).exists():
             liked = True
-        
+
         return render(
             request,
             "routine_detail.html",
