06ec0c08185fdbe9def1603b787c7de27e9c6eb3
Sorin05 <sorinandrei230587@gmail.com>
2022-07-17 11:08:09 +0000

    goal create function

--- goals/views.py
+++ goals/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Goal
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
 def dashboard(request):
-    return render(request, 'goals/dashboard.html')
+    goals = Goal.objects.filter(user=request.user)
+    
+    if request.method == 'POST':
+        goal = request.POST.get('goal')
+        Goal.objects.get_or_create(body=goal, user=request.user)
+        return redirect('dashboard')
+    return render(request, 'goals/dashboard.html', {'goals': goals})
